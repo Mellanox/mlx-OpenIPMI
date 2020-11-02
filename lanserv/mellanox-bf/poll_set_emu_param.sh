05a1e5a8989000279633748bd9c60c86fcf12245
Asmaa Mnebhi <asmaa@nvidia.com>
2020-11-02 12:36:30 -0500

    support IPMI via OOB
    https://redmine.mellanox.com/issues/2330349
    
    The above is a request to add support for IPMI
    via OOB. Now, the user can issue a command from
    the external host to the BF via lanplus and get
    a response back:
    ipmitool -I lanplus -H 192.168.101.2 -U ADMIN -P ADMIN sensor list
    
    Since this is only supported on certain BF-2 systems,
    the user can enable this feature by simply setting
    OOB_IP variable in progconf. For example:
    OOB_IP="192.168.101.2"
    By default, OOB is not supported, so OOB_IP="0".

--- lanserv/mellanox-bf/poll_set_emu_param.sh
+++ lanserv/mellanox-bf/poll_set_emu_param.sh
@@ -4,8 +4,11 @@
 # $1 time interval for executing set_emu_param.sh in seconds.
 # $2 is set to "Bluewhale" if it is a BlueWhale board.
 # $3 should be set to 1 if IPMB is supported.
+# $4 should be set to the OOB ip address if supported.
+# example: "192.168.101.2". If OOB is not supported, $4
+# should be set to "0".
 
 while /bin/true; do
-	/usr/bin/set_emu_param.sh $2 $3
+	/usr/bin/set_emu_param.sh $2 $3 $4
 	sleep $1
 done
