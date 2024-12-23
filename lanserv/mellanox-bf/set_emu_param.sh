4f27c8aaa6434b807fe8f5f428c281a96d11645c
Yuval Nissan <ynissan@nvidia.com>
2024-12-23 02:29:49 -0800

    Adding RTC battery voltage sensor to SDR list
    Sensor name is "rtc_voltage" and is derived from sysfs. Sensor is discrete with two value, 0x0 and 0x80, which correspond to expected and low voltage, respectively. The values are mapped to their corresponding voltage values, 3V and 2V, respectively, so they can be represented as a non-discrete sensor (supported by the Redfish schema). A critical lower threshold is set in 2.3V to allow assertion in case of low voltage.
    
    '''
    Test 1:
    
    ...
    rtc_voltage      | 3.000      | Volts      | ok    | na        | 2.300     | na        | na        | na
    
    Test 2:
    
    ...
    rtc_voltage      | 3.000      | Volts      | ok    | na        | 2.302     | na        | na        | na
    
    Test 3:
    
    {
        "@odata.id": "/redfish/v1/Chassis/Card1/Sensors",
        "@odata.type": "#SensorCollection.SensorCollection",
        "Description": "Collection of Sensors for this Chassis",
        "Members": [
            ...
            {
                "@odata.id": "/redfish/v1/Chassis/Card1/Sensors/rtc_voltage"
            }
        ],
        "Members@odata.count": 21,
        "Name": "Sensors"
    }
    
    Test 4:
    
    {
        "@odata.id": "/redfish/v1/Chassis/Card1/Sensors/rtc_voltage",
        "@odata.type": "#Sensor.v1_7_0.Sensor",
        "Id": "rtc_voltage",
        "Name": "rtc voltage",
        "Reading": 3.0,
        "ReadingRangeMax": 3.0,
        "ReadingRangeMin": 2.0,
        "ReadingType": "Voltage",
        "ReadingUnits": "V",
        "RelatedItem": [
            {
                "@odata.id": "/redfish/v1/Systems/Bluefield"
            }
        ],
        "Status": {
            "Conditions": [],
            "Health": "OK",
            "HealthRollup": "OK",
            "State": "Enabled"
        },
        "Thresholds": {
            "LowerCritical": {
                "Reading": 2.3
            }
        }
    }
    '''
    
    Fixes jira https://redmine.mellanox.com/issues/4016381
    
    Signed-off-by: Yuval Nissan <ynissan@nvidia.com>

--- lanserv/mellanox-bf/set_emu_param.sh
+++ lanserv/mellanox-bf/set_emu_param.sh
@@ -661,6 +661,39 @@ else
     remove_sensor "ddr_temp"
 fi
 
+####################################
+#    Get the RTC Battry Voltage    #
+####################################
+
+# Read discrete RTC low battery voltage value.
+# A value of 0x0 means RTC battery voltage is good.
+# A value of 0x80 means RTC battery voltage is low.
+temp=$(cat /sys/devices/platform/MLNXBF04:00/rtc_battery)
+
+# Currently, there is no Redfish schema which supports discrete sensors.
+# Hence, we map the values to the corresponding voltages which they represent.
+# We need the sensor resolution in volts to be 0.1V, because the thresholds
+# can have non-integer values in that resolution.
+# Since the SDR sensor configuration is based on raw integer values, we set
+# the input to be 10 times the real value in volts we want displayed.
+# A good voltage is considered to be ~3 volts.
+# So, 0x0 will be mapped to 30 (10 times 3).
+# A low voltage is considered to ve ~2 volts.
+# So, 0x80 will be mapped to 20 (10 times 2).
+
+case "$temp" in
+    0x0)
+        echo "30" > "$EMU_PARAM_DIR/rtc_voltage"
+        ;;
+    0x80)
+        echo "20" > "$EMU_PARAM_DIR/rtc_voltage"
+        ;;
+    *)
+        echo "RTC battery low voltage returned error code $temp"
+        remove_sensor "rtc_voltage"
+        ;;
+esac
+
 ###################################
 #          Get FW info            #
 ###################################
